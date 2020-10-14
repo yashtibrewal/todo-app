b759a9d98b41599098147ed88c8c97f9ec8a03e4
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2020-10-14 22:08:26 +0530

    added success and fail

--- src/abstracts/ApiResponse.ts
+++ src/abstracts/ApiResponse.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from "express";
+import { ApiResponseStructure } from "../interfaces";
 
 abstract class ApiResponse {
 
-    async sendResponse(req: Request, res: Response) {
+    async fail(errors: Error[]): Promise<ApiResponseStructure> {
+        return {
+            success: false,
+            data: {},
+            errors,
+        }
+    }
+
+    async success(data: any): Promise<ApiResponseStructure> {
+        return {
+            success: true,
+            data: data,
+            errors: [],
+        }
+    }
+
+    async sendResponse(res: Response) {
         res.send(res.locals.response);
     }
 
