4d8cf3e3e531ca0615f15f16fc6b584e297494b6
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2020-10-14 22:01:31 +0530

    created general errors

---
+++ src/helpers/GeneralErrors.ts
@@ -0,0 +1,25 @@
+class InvalidDataType implements Error {
+    name = 'InvalidDataType';
+    message: string;
+    constructor(field: string, expected_type: string) {
+        this.message = `Invalid Data Type Detected for the field ${field}, expected ${expected_type}`;
+    }
+}
+
+class InvalidValue implements Error {
+    name = "InvalidValue";
+    message: string;
+    constructor(field: string) {
+        this.message = `Invalid value found for the field ${field}`;
+    }
+}
+
+class NotFound implements Error {
+    name = "NotFound";
+    message: string;
+    constructor(field: string) {
+        this.message = `The field ${field} was expected`;
+    }
+}
+
+export { InvalidDataType, InvalidValue, NotFound }
